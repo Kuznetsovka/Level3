d33a843f8ceb2bc7bc67100abb9f9a166d59fa9b
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-04-06 23:54:32 +0300

    ДЗ к уроку 2. 2-я часть.

--- src/main/java/lesson2/WorkWithDatabase.java
+++ src/main/java/lesson2/WorkWithDatabase.java
@@ -1,10 +1,13 @@
 package lesson2;
 
+import java.io.*;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class WorkWithDatabase {
     private static Connection connection;
     private static Statement statement;
+    private static final String URL = "src/main/java/lesson2/";
 
     public static void main(String[] args) {
         try {
@@ -19,6 +22,17 @@ public class WorkWithDatabase {
             deleteColumnByName("STUDENTS","Bob4");
             updateScoreByName("STUDENTS",100,"Bob5");
             selectSql("STUDENTS","StudID","NAME","SCORE");
+            String[][] idNameScore;
+
+            ArrayList<String> updateData  = UpdateDataFromTxt(URL + "DZ_update.txt");
+            int i = 0;
+            idNameScore = splitArrayList(updateData, i);
+            for (int j = 0; j <idNameScore.length ; j++) {
+                //updateScoreByName("STUDENTS", Integer.parseInt(idNameScore[j][2]),idNameScore[j][1]);
+                updateScoreByID("STUDENTS", Integer.parseInt(idNameScore[j][2]),
+                        Integer.parseInt(idNameScore[j][0]),idNameScore[j][1]);
+                //Думаю так будет праввильнее, вдруг есть тески
+            }
             disconnect();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -27,9 +41,20 @@ public class WorkWithDatabase {
         }
     }
 
+    private static String[][] splitArrayList(ArrayList<String> updateData, int i) {
+        String[][] res;
+        res = new String[countWords(updateData.get(0))-1][updateData.size()];
+        for (String updateDatum : updateData) {
+            String []temp = updateDatum.split("\\s+");
+            res[i] = temp;
+            i++;
+        }
+        return res;
+    }
+
     static void connect() throws ClassNotFoundException, SQLException {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:src/main/java/lesson2/studentBase.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + URL + "studentBase.db");
             statement = connection.createStatement();
     }
 
@@ -55,7 +80,7 @@ public class WorkWithDatabase {
         return null;
     }
 
-    static void insertSql(String table,String[]... name_ValueColumn) {
+    static int insertSql(String table,String[]... name_ValueColumn) {
         String nameColumns = "";
         String valueColumns = "";
         for (String[] strings : name_ValueColumn) {
@@ -66,18 +91,37 @@ public class WorkWithDatabase {
         valueColumns = valueColumns.substring(3);
         String query = String.format("INSERT INTO %s (%s) VALUES ('%s');",table,nameColumns,valueColumns);
         try {
-            statement.execute(query);
+            int res = statement.executeUpdate(query);
             info(query);
+            return res;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void updateScoreByName(String table, int score, String name){
+    private static int updateScoreByName(String table, int score, String name){
         String query = String.format("UPDATE '%s' SET Score = '%d' WHERE Name ='%s';",table,score,name);
         try {
-            statement.execute(query);
-            info(query);
+            int res = statement.executeUpdate(query);
+                if (res>=1)
+                    info(query);
+                else
+                    insertSql("STUDENTS", new String[]{"NAME", name}, new String[]{"SCORE", String.valueOf(score)});
+            return res;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int updateScoreByID(String table, int score, int id,String name){
+        String query = String.format("UPDATE '%s' SET Score = '%d' WHERE StudID = '%d' AND Name ='%s';",table,score,id,name);
+        try {
+            int res = statement.executeUpdate(query);
+            if (res>=1)
+                info(query);
+            else
+                insertSql("STUDENTS", new String[]{"NAME", name}, new String[]{"SCORE", String.valueOf(score)});
+            return res;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -97,17 +141,55 @@ public class WorkWithDatabase {
         System.out.println("Выполнен запрос " + query);
     }
 
-    private static void createTable(String table, String nameColumn1, String nameColumn2){
+    private static int createTable(String table, String nameColumn1, String nameColumn2){
         String query = String.format("CREATE TABLE IF NOT EXISTS '%s' (" +
                 "StudID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
                 "'%s' TEXT NOT NULL," +
                 "'%s' INTEGER NOT NULL);",table,nameColumn1,nameColumn2);
         try {
-            statement.execute(query);
+            int res = statement.executeUpdate(query);
             info(query);
+            return res;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+    private static ArrayList<String> UpdateDataFromTxt(String url){
+        ArrayList<String> res = new ArrayList<>();
+        try {
+            File file = new File(url);
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            while (line != null) {
+                if (isDigit(line.charAt(0)))
+                    res.add(line);
+                line = reader.readLine();
+            }
+            return res;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean isDigit(char chr){
+        return Character.isDigit(chr);
+    }
+
+    public static int countWords(String txt){
+        int count = 0;
+        if(txt.length() != 0){
+            count++;
+            for (int i = 0; i < txt.length(); i++) {
+                if(txt.charAt(i) == ' '){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
-//1. Сделать методы для работы с БД (CREATE, DELETE)
